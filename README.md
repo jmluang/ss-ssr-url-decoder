1d8c12202686639dce96cb210fe236e1b503e682
jmluang <350216438@qq.com>
2018-12-07 10:39:46 +0800

    update readme and example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ss-ssr-url-decoder
 Ss url or Ssr url decode and generate QR code.
+
+# Install
+`composer require jmluang\ssr`
+
+# Useage
+see [example.php](https://github.com/jmluang/ss-ssr-url-decoder/blob/master/example.php) file for more details
+
+# LICENSE
+MIT
--- example.php
+++ example.php
@@ -2,9 +2,13 @@
 require 'vendor/autoload.php';
 
 use jmluang\ssr\Decoder;
+use jmluang\ssr\Exception\DecodeException;
 
 $ssr = (new Decoder('ssr://MTI3LjAuMC4xOjEyMzQ6YXV0aF9hZXMxMjhfbWQ1OmFlcy0xMjgtY2ZiOnRsczEuMl90aWNrZXRfYXV0aDpZV0ZoWW1KaS8_b2Jmc3BhcmFtPVluSmxZV3QzWVRFeExtMXZaUSZyZW1hcmtzPTVyV0w2Sy1WNUxpdDVwYUg'))->decode();
 print_r((array)$ssr);
 
-$ss = (new Decoder('ss://YmYtY2ZiOnRlc3RAMTkyLjE2OC4xMDAuMTo4ODg4Cg'))->decode();
-print_r((array)$ss);
+try {
+    $ss = (new Decoder('ss://'))->decode();
+} catch (DecodeException $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
